fcb491385982b03c9a5d2dce74557e4834fe65f7
Patrick Balestra <me@patrickbalestra.com>
2018-03-21 10:02:40 +0100

    Hack code to adapt for very long names

--- BadgesGenerator.playground/Contents.swift
+++ BadgesGenerator.playground/Contents.swift
@@ -35,7 +35,7 @@ public func render(_ attendee: Attendee) -> Data {
             let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
             let height = attributedString.boundingRect(with: CGSize(width: rect.width, height: 750), options: [.usesLineFragmentOrigin], context: nil).height
             let lines = Int(height / optimalFont)
-            if lines == 2 {
+            if lines >= 2 {
                 // Check if we can fit everything into one line by decreasing font size
                 let newFont = CGFloat(15.0)
                 attributedString.setAttributes([NSAttributedStringKey.font: UIFont(name: calibri, size: newFont)!, NSAttributedStringKey.paragraphStyle: paragraphStyle, NSAttributedStringKey.foregroundColor: #colorLiteral(red: 0.1843137255, green: 0.1843137255, blue: 0.1843137255, alpha: 0.7)], range: NSMakeRange(0, string.count))
@@ -52,12 +52,17 @@ public func render(_ attendee: Attendee) -> Data {
             let fontSize = CGFloat(24)
             let attributes = [NSAttributedStringKey.font: UIFont(name: calibriBold, size: fontSize)!, NSAttributedStringKey.paragraphStyle: paragraphStyle, NSAttributedStringKey.foregroundColor: #colorLiteral(red: 0.2470588235, green: 0.2470588235, blue: 0.2470588235, alpha: 1)]
             let string = "\(attendee.firstName)\n\(attendee.lastName)"
-            let attributedString = NSAttributedString(string: string, attributes: attributes)
+            let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
             let height = attributedString.boundingRect(with: CGSize(width: rect.width, height: 750), options: [.usesLineFragmentOrigin], context: nil).height
-            let lines = Int(height / fontSize)
             var yPosition = CGFloat(105)
             if yPositionCompany != 155 {
                 yPosition = CGFloat(100)
+            }
+            let lines = Int(height / fontSize)
+            if lines >= 2 {
+                // Check if we can fit everything into one line by decreasing font size
+                let newFont = CGFloat(20.0)
+                attributedString.setAttributes([NSAttributedStringKey.font: UIFont(name: calibriBold, size: newFont)!, NSAttributedStringKey.paragraphStyle: paragraphStyle, NSAttributedStringKey.foregroundColor: #colorLiteral(red: 0.2470588235, green: 0.2470588235, blue: 0.2470588235, alpha: 1)], range: NSMakeRange(0, string.count))
             }
             attributedString.draw(with: CGRect(x: 0, y: yPosition, width: rect.width, height: rect.height), options: .usesLineFragmentOrigin, context: nil)
         }
